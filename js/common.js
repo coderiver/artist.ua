73c498df6d524a4a548cf2f74b1a41009b34d73d
Artur Moroz <morozarturv@gmail.com>
2013-08-08 17:54:53 +0300

    all pages

--- js/common.js
+++ js/common.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
-
+//about accordeon
+function accordeon() {
+	var el = $('.js-about-accos');
+	var btn = el.find('button');
+	var row = el.find('.about__row');
+	row.hide();
+	el.first().find('.about__row').show();
+	el.first().find('button').addClass('is-active');
+	btn.click(function() {
+		if (!$(this).hasClass('is-active')) {
+			row.slideUp();
+			btn.removeClass('is-active');
+			$(this).next().slideDown();
+			$(this).addClass('is-active');
+		};
+	});
+};
+accordeon();
 
 });
